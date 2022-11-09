2a86c4afd296ac25d715fdc57d435713bfe76ad2
EwelinaManiecka <ewelina.maniecka@op.pl>
2022-11-09 16:12:43 +0100

    Add div
    Add div id for clickBtn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const visibilityBtn = document.getElementById("show");
 const visibilityIcon = document.getElementById("hide");
+const clickedIcon = document.getElementById("clicked-icon");
 
-visibilityBtn.addEventListener("click", toggleVisibility);
+clickedIcon.addEventListener("click", toggleVisibility);
 
 function toggleVisibility() {
-    const passwordInput = document.getElementById("password");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        document.getElementById("hide").style.display = "inline-block";
-        document.getElementById("show").style.display = "none";
-    } else {
-        passwordInput.type = "password";
-        document.getElementById("hide").style.display = "none";
-        document.getElementById("show").style.display = "inline-block";
-    };
-};
+  const passwordInput = document.getElementById("password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    document.getElementById("hide").style.display = "inline-block";
+    document.getElementById("show").style.display = "none";
+  } else {
+    passwordInput.type = "password";
+    document.getElementById("hide").style.display = "none";
+    document.getElementById("show").style.display = "inline-block";
+  }
+}
 
 // const showPassword = document.querySelector("#show");
 // const passwordField = document.querySelector("#password");
